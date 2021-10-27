6668bc447711f1bafba20a6e48eae46424fcdf42
Alisher Togayev <38793933+Alisherka7@users.noreply.github.com>
2021-10-27 17:44:29 +0900

    Update bubbleS.java

--- Java/BubbleSort/bubbleS.java
+++ Java/BubbleSort/bubbleS.java
@@ -1,1 +1,28 @@
+class bubbleS {
 
+    public static void bubbleSort(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length - (i + 1); j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] list = { 4, 5, 2, 1, 9, 7, 6 };
+        System.out.println("Unsorted array");
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+        bubbleSort(list);
+        System.out.println("\n\nSelection Sorted Array");
+        for (int i : list) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
